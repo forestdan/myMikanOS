4fe19e070d6785aad01c5e6376c6cf350e26a0fc
forestdan <dandancc007@qq.com>
2021-02-02 18:16:11 +0900

    9.1 finished

--- kernel/console.hpp
+++ kernel/console.hpp
@@ -3,17 +3,19 @@
 #include "graphics.hpp"
 
 class Console {
-    public:
-        static const int kRows = 25, kColumns = 80;
-        
-        Console(PixelWriter& writer, const PixelColor& fg_color, const PixelColor& bg_Color);
-        void PutString(const char*s);
-    
-    private:
-        void NewLine();
+ public:
+  static const int kRows = 25, kColumns = 80;
 
-        PixelWriter& writer_;
-        const PixelColor fg_color_, bg_Color_;
-        char buffer_[kRows][kColumns + 1];
-        int cursor_row_, cursor_colunm_;
+  Console(const PixelColor& fg_color, const PixelColor& bg_color);
+  void PutString(const char* s);
+  void SetWriter(PixelWriter* writer);
+
+ private:
+  void Newline();
+  void Refresh();
+
+  PixelWriter* writer_;
+  const PixelColor fg_color_, bg_color_;
+  char buffer_[kRows][kColumns + 1];
+  int cursor_row_, cursor_column_;
 };
--- kernel/mouse.hpp
+++ kernel/mouse.hpp
@@ -9,15 +9,12 @@
 // #@@range_begin(mouse_class)
 #include "graphics.hpp"
 
-class MouseCursor {
- public:
-  MouseCursor(PixelWriter* writer, PixelColor erase_color,
-              Vector2D<int> initial_position);
-  void MoveRelative(Vector2D<int> displacement);
+#pragma once
 
- private:
-  PixelWriter* pixel_writer_ = nullptr;
-  PixelColor erase_color_;
-  Vector2D<int> position_;
-};
-// #@@range_end(mouse_class)
+#include "graphics.hpp"
+
+const int kMouseCursorWidth = 15;
+const int kMouseCursorHeight = 24;
+const PixelColor kMouseTransparentColor{0, 0, 1};
+
+void DrawMouseCursor(PixelWriter* pixel_writer, Vector2D<int> position);
