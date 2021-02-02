ef6afd0d0f22cd0a09eaba84a71fd9f179b5356a
forestdan <dandancc007@qq.com>
2021-02-02 19:11:44 +0900

    9.5 finished

--- kernel/font.hpp
+++ kernel/font.hpp
@@ -1,9 +1,7 @@
 #pragma once
 
-#include "graphics.hpp"
 #include <cstdint>
+#include "graphics.hpp"
 
-
-void WriteAscii(PixelWriter& writer, int x, int y, char c, const PixelColor& color);
-
-void WriteString(PixelWriter& writer, int x, int y, const char* str, const PixelColor& color);
+void WriteAscii(PixelWriter& writer, Vector2D<int> pos, char c, const PixelColor& color);
+void WriteString(PixelWriter& writer, Vector2D<int> pos, const char* s, const PixelColor& color);
